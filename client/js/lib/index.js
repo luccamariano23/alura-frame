10a1c5102264dcbb9b7ee0c230cc3fd303126540
Lucca Mariano <luccaricardomariano@hotmail.com>
2021-02-06 21:48:50 -0300

    initial commit

---
+++ client/js/lib/index.js
@@ -0,0 +1,41 @@
+//javascript root
+var campos = [
+    document.querySelector('#data'),
+    document.querySelector('#quantidade'),
+    document.querySelector('#valor')
+];
+
+//valor criado fora do 'form submit', não tem a necessidade de instaciar N vezes
+var tbody = document.querySelector(' table tbody');
+
+document.querySelector(".form").addEventListener('submit',function (event) {
+
+    event.preventDefault();
+
+    var tr = document.createElement('tr');
+
+    campos.forEach(function (campo) {
+        var td = document.createElement('td');
+        td.textContent = campo.value;
+        tr.appendChild(td);
+    });
+
+    var tdVolume = document.createElement('td');
+    tdVolume.textContent = campos[1].value * campos[2].value;
+
+    tr.appendChild(tdVolume);
+
+    tbody.appendChild(tr);
+
+    campos[0].value='';
+    campos[1].value=1;
+    campos[2].value=0;
+
+    campos[0].focus();
+
+});
+
+//jquery
+// $("#id_do_form").on('submit', function (){
+//
+// });
